49607ef7a2840007f3c9fc74ab83f2bb978bfa84
Kosnstantin <tryvoidmain@gmail.com>
2021-05-26 22:01:24 +0300

    Добавил поле ExpirationDate в сущность, будем в него складывать срок годности каждой сущности лекарства.

--- Backend/domain/PharmacyHome/Medicine.cs
+++ Backend/domain/PharmacyHome/Medicine.cs
@@ -1,15 +1,19 @@
-﻿
+﻿using System;
+
+
 namespace PharmacyHome
 {
     public class Medicine
     {
         public int Id { get; }
+        public string ExpirationDate { get; }
         public string Title { get; }
 
-        public Medicine(int id, string title)
+        public Medicine(int id, string title, string expirationDate)
         {
-            Id = id;
+            Id = id;            
             Title = title;
+            ExpirationDate = expirationDate;
         }
 
     }
