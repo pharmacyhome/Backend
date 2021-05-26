ab236d6d79e2f0972043ae33c09bb2ce291b7077
Kosnstantin <tryvoidmain@gmail.com>
2021-05-26 22:07:46 +0300

    Т.к. у нас появилось два метода поиска в IMedicineRepository, то создаём класс MedicineService, в котором в дальнейшем будет заложена логика выбора одного из этих методов. В SearchController привели всё в соответствие к этой структуре.

---
+++ Backend/domain/PharmacyHome/MedicineService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyHome
+{
+    public class MedicineService
+    {
+        public Medicine[] GetAllByQuery(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Backend/presentation/PharmacyHome.Web/Controllers/SearchController.cs
+++ Backend/presentation/PharmacyHome.Web/Controllers/SearchController.cs
@@ -9,15 +9,16 @@ namespace PharmacyHome.Web.Controllers
        
     public class SearchController : Controller
     {
-        private readonly IMedicineRepository medicineRepository;
-        public SearchController(IMedicineRepository medicineRepository)
+        private readonly MedicineService medicineService;
+        public SearchController(MedicineService medicineService)
         {
-            this.medicineRepository = medicineRepository;
+            this.medicineService = medicineService;
         }
 
         public IActionResult Index(string query)
         {
-            var medicine = medicineRepository.GetAllByTitle(query);
+            var medicine = medicineService.GetAllByQuery(query);
+
             return View(medicine);
         }
     }
