e78419b53ec9075fc0c84a886364854240c1dc5a
Konstantin Shcherbinin <tryvoidmain@gmail.com>
2022-09-10 23:21:24 +0300

    Добавил конфигурацию в Startup и зарегистрировал сервисы в DI

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,7 @@
-﻿namespace PharmacyHome
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace PharmacyHome
 {
     public class Startup
     {
@@ -10,12 +13,48 @@
         }
 
         public void ConfigureServices(IServiceCollection services)
-        {
+        {           
+            services.AddControllers().AddNewtonsoftJson(options =>
+            {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                options.SerializerSettings.Formatting = Formatting.Indented;
+                JsonConvert.DefaultSettings = () => options.SerializerSettings;
+            });
+            services.AddMvc().AddControllersAsServices();
 
+            bool useSSL = configuration.GetValue<bool>("UseSSL");
+            if (useSSL)
+            {
+                int httpsPort = configuration.GetValue<int>("HttpsPort");
+                services.AddHttpsRedirection(options =>
+                {
+                    options.HttpsPort = httpsPort;
+                });
+            }
+
+            services.AddControllersWithViews();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostBuilder env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
 
         }
     }
