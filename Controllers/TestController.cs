0421cebe0905d44f0df2c8614c63e19d8da359e3
Konstantin Shcherbinin <tryvoidmain@gmail.com>
2022-09-11 18:04:40 +0300

    Тестовый контроллер для проверки Rest запросов

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PharmacyHome.Controllers
+{
+    [ApiController]
+    public class TestController
+    {      
+        [HttpGet("/api/testresult")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(object))]
+        public object GetTestData()
+        {
+            return new { Name = "Name", Value = "Value" };
+        }
+    }
+}
